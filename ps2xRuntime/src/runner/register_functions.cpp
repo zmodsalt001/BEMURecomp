4b9879ea1441cd18130f2590859d453cf6c35ce9
Z, from Z Mods <71144908+zmodsalt001@users.noreply.github.com>
2026-09-04 17:42:38 -0300

    Update register_functions.cpp

--- ps2xRuntime/src/runner/register_functions.cpp
+++ ps2xRuntime/src/runner/register_functions.cpp
@@ -1,6 +1,6 @@
 #include "ps2_runtime.h"
 #include "runtime/ps2_memory.h"
-
+//e
 extern const uint32_t g_ps2RecompiledFunctionTableBase = 0x00000000u;
 extern const uint32_t g_ps2RecompiledFunctionTableEnd = 0x01000000u;
 extern const uint32_t g_ps2RecompiledFunctionTableSlotCount = (g_ps2RecompiledFunctionTableEnd - g_ps2RecompiledFunctionTableBase) >> 2;
